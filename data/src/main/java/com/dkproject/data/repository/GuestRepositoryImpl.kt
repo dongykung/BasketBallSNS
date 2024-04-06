072e493a7b1b357634b07b2528e5948ec12b633b
dongkyung <kdkd422@naver.com>
2024-04-06 18:42:09 +0900

    GuestRepository

--- data/src/main/java/com/dkproject/data/repository/GuestRepositoryImpl.kt
+++ data/src/main/java/com/dkproject/data/repository/GuestRepositoryImpl.kt
@@ -1,16 +1,20 @@
 package com.dkproject.data.repository
 
+import android.util.Log
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import com.dkproject.data.model.GuestDTO
 import com.dkproject.data.pagingDataSource.GuestDataSource
+import com.dkproject.data.pagingDataSource.NearGuestDataSource
 import com.dkproject.domain.common.Resource
+import com.dkproject.domain.model.UserInfo
 import com.dkproject.domain.model.home.Guest
 import com.dkproject.domain.repository.GuestRepository
 import com.firebase.geofire.GeoFireUtils
 import com.firebase.geofire.GeoLocation
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.tasks.await
 import java.util.Calendar
@@ -46,7 +50,7 @@ class GuestRepositoryImpl @Inject constructor(private val firestore: FirebaseFir
         }
     }
 
-    override suspend fun getGuest(position: String,date:Boolean): Flow<PagingData<Guest>> =
+    override suspend fun getGuest(position: String,date:Long): Flow<PagingData<Guest>> =
         Pager(
             config = PagingConfig(
                 pageSize = 10,
@@ -54,5 +58,24 @@ class GuestRepositoryImpl @Inject constructor(private val firestore: FirebaseFir
             ),
             pagingSourceFactory = { GuestDataSource(firestore,position,date)}
         ).flow
+
+    override suspend fun getNearGuest(position: String, date: Long): Flow<PagingData<Guest>>  =
+        Pager(
+            config = PagingConfig(
+                pageSize = 10,
+                enablePlaceholders = false
+            ),
+            pagingSourceFactory = { NearGuestDataSource(firestore,position,date)}
+        ).flow
+
+    override suspend fun getGuestItem(uid: String): Resource<Guest> {
+        return try {
+            val query = firestore.collection("Guest").document(uid).get().await()
+            val data = query.toObject(GuestDTO::class.java)?.toDomainModel()!!
+            Resource.Success(data)
+        } catch (e: Exception) {
+            Resource.Error(e.message.toString())
+        }
+    }
 }
 
