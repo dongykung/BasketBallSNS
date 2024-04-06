ac669d55eb201632d909ff85068deb651eba3d74
dongkyung <kdkd422@naver.com>
2024-04-06 18:41:32 +0900

    data-datadto

--- data/src/main/java/com/dkproject/data/model/UserDTO.kt
+++ data/src/main/java/com/dkproject/data/model/UserDTO.kt
@@ -3,10 +3,21 @@ package com.dkproject.data.model
 import com.dkproject.domain.model.UserInfo
 
 data class UserDTO(
-    val userid:String,
-    val username:String,
-    val profileImageUrl:String,
-    val first:Boolean
+    val useruid:String="",
+    val nickname: String="",
+    val profileImageUrl: String="",
+    val playPosition:List<String> = emptyList(),
+    val playStyle:List<String> = emptyList(),
+    val userSkill:String = "",
 ){
-
+    fun toDomainModel():UserInfo{
+        return UserInfo(
+            useruid=useruid,
+         nickname=nickname,
+         profileImageUrl=profileImageUrl,
+         playPosition=playPosition,
+         playStyle=playStyle,
+         userSkill=userSkill
+        )
+    }
 }
--- domain/src/main/java/com/dkproject/domain/model/UserInfo.kt
+++ domain/src/main/java/com/dkproject/domain/model/UserInfo.kt
@@ -1,10 +1,10 @@
 package com.dkproject.domain.model
 
 data class UserInfo(
-    val useruid:String,
-    val nickname: String,
-    val profileImageUrl: String,
-    val playPosition:List<String>,
-    val playStyle:List<String>,
-    val userSkill:String,
+    val useruid:String,  //
+    val nickname: String, //
+    val profileImageUrl: String, //
+    val playPosition:List<String>, //
+    val playStyle:List<String>, //
+    val userSkill:String, //
 )
