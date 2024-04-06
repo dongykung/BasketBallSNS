45cd19a058dbbafd30b36f5b8955049615d407d5
dongkyung <kdkd422@naver.com>
2024-04-06 18:40:53 +0900

    di-GuestModule

--- data/src/main/java/com/dkproject/data/di/GuestModule.kt
+++ data/src/main/java/com/dkproject/data/di/GuestModule.kt
@@ -1,9 +1,14 @@
 package com.dkproject.data.di
 
 import com.dkproject.data.repository.GuestRepositoryImpl
+import com.dkproject.data.repository.UserRepositoryImpl
 import com.dkproject.domain.repository.GuestRepository
+import com.dkproject.domain.repository.UserRepository
+import com.dkproject.domain.usecase.home.GetGuestItemUseCase
 import com.dkproject.domain.usecase.home.GetGuestUseCase
+import com.dkproject.domain.usecase.home.GetNearGuestUseCase
 import com.dkproject.domain.usecase.home.UploadGuestUseCase
+import com.dkproject.domain.usecase.user.GetUserInfoUseCase
 import com.google.firebase.firestore.FirebaseFirestore
 import dagger.Binds
 import dagger.Module
@@ -33,6 +38,29 @@ object GuestModule2{
     @Singleton
     fun provideloadGuestUseCase(guestTestRepository: GuestRepository):GetGuestUseCase{
         return GetGuestUseCase(guestTestRepository)
+    }
+    @Provides
+    @Singleton
+    fun provideloadNeqrGuestUseCase(guestTestRepository: GuestRepository):GetNearGuestUseCase{
+        return GetNearGuestUseCase(guestTestRepository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideloadGuestItemUseCase(guestTestRepository: GuestRepository):GetGuestItemUseCase{
+        return GetGuestItemUseCase(guestTestRepository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideUserRepository(firestore: FirebaseFirestore):UserRepository{
+        return UserRepositoryImpl(firestore)
+    }
+
+    @Provides
+    @Singleton
+    fun provideUserUseCase(userRepositoryImpl: UserRepositoryImpl):GetUserInfoUseCase{
+        return GetUserInfoUseCase(userRepositoryImpl)
     }
 
 }
