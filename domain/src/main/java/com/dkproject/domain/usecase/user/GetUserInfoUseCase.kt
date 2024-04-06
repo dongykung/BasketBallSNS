4bb340e70d1eb21705523c4ca317b2ac4933978e
dongkyung <kdkd422@naver.com>
2024-04-06 18:39:30 +0900

    Domain-GetUserInfoUseCase

--- domain/src/main/java/com/dkproject/domain/usecase/user/GetUserInfoUseCase.kt
+++ domain/src/main/java/com/dkproject/domain/usecase/user/GetUserInfoUseCase.kt
@@ -1,5 +1,19 @@
 package com.dkproject.domain.usecase.user
 
-interface GetUserInfoUseCase {
-    suspend operator fun invoke()
+import com.dkproject.domain.common.Resource
+import com.dkproject.domain.model.UserInfo
+import com.dkproject.domain.repository.UserRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class GetUserInfoUseCase(private val userRepository: UserRepository) {
+
+   suspend operator fun invoke(uid:String) : Flow<Resource<UserInfo>> = flow{
+      try {
+          emit(Resource.Loading())
+          emit(userRepository.getUserInfo(uid))
+      }catch (e:Exception){
+          emit(Resource.Error(e.message.toString()))
+      }
+   }
 }
