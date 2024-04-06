fd8e81fb19e5fa983eb0f61b0721f9e3d258451d
dongkyung <kdkd422@naver.com>
2024-04-06 18:39:48 +0900

    Data-GetUserInfoUseCaseImpl

--- data/src/main/java/com/dkproject/data/usecase/user/GetUserInfoUseCaseImpl.kt
+++
@@ -1,9 +0,0 @@
-package com.dkproject.data.usecase.user
-
-import com.dkproject.domain.usecase.user.GetUserInfoUseCase
-
-class GetUserInfoUseCaseImpl : GetUserInfoUseCase {
-    override suspend fun invoke() {
-        TODO("Not yet implemented")
-    }
-}
