8e748e1d028ada6e652c351b642d7b6a28251953
dongkyung <kdkd422@naver.com>
2024-04-06 18:42:19 +0900

    Login

--- presentation/src/main/java/com/dkproject/presentation/ui/activity/LoginActivity.kt
+++ presentation/src/main/java/com/dkproject/presentation/ui/activity/LoginActivity.kt
@@ -1,10 +1,12 @@
 package com.dkproject.presentation.ui.activity
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.dkproject.presentation.ui.screen.login.LoginScreen
 import com.dkproject.presentation.ui.theme.BasketballSNSTheme
+import com.kakao.sdk.common.util.Utility
 import dagger.hilt.android.AndroidEntryPoint
 
 
