b927847b5310237d7b09ab815da8a96d5548ef4f
dongkyung <kdkd422@naver.com>
2024-04-06 18:37:39 +0900

    kakao_sdk_init

--- app/src/main/java/com/dkproject/basketballsns/ui/theme/App.kt
+++ app/src/main/java/com/dkproject/basketballsns/ui/theme/App.kt
@@ -1,10 +1,16 @@
 package com.dkproject.basketballsns.ui.theme
 
 import android.app.Application
+import android.util.Log
+import com.dkproject.presentation.BuildConfig
+import com.kakao.sdk.common.KakaoSdk
 import dagger.hilt.android.HiltAndroidApp
 
 
 @HiltAndroidApp
 class App:Application() {
-
+    override fun onCreate() {
+        super.onCreate()
+        KakaoSdk.init(this,BuildConfig.KAKAO_API_KEY)
+    }
 }
