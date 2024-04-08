b7f558c60f1d9fc03147155ac1b28b63e99a61f7
dongkyung <kdkd422@naver.com>
2024-04-09 00:02:52 +0900

    LoginViewModel

--- presentation/src/main/java/com/dkproject/presentation/ui/screen/login/LoginViewModel.kt
+++ presentation/src/main/java/com/dkproject/presentation/ui/screen/login/LoginViewModel.kt
@@ -1,7 +1,7 @@
 package com.dkproject.presentation.ui.screen.login
 
+import android.content.Context
 import android.util.Log
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -11,9 +11,15 @@ import androidx.lifecycle.viewModelScope
 import com.dkproject.domain.usecase.login.CheckFirstUseCase
 import com.dkproject.domain.usecase.token.GetTokenUseCase
 import com.dkproject.domain.usecase.token.SetTokenUseCase
+import com.dkproject.presentation.ui.component.showToastMessage
+import com.dkproject.presentation.util.Constants
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.firebase.auth.AuthCredential
 import com.google.firebase.auth.FirebaseAuth
+import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.common.model.ClientError
+import com.kakao.sdk.common.model.ClientErrorCause
+import com.kakao.sdk.user.UserApiClient
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
@@ -31,18 +37,22 @@ class LoginViewModel @Inject constructor(
     companion object {
         const val TAG = "LoginViewModel"
     }
-   private val _autoLogin = mutableStateOf(false)
-    val autoLogin :State<Boolean> = _autoLogin
+
+    private val _autoLogin = mutableStateOf(false)
+    val autoLogin: State<Boolean> = _autoLogin
 
     val googleClientIntent = googleClient.signInIntent
 
     init {
         autoLogin()
     }
-    fun autoLogin(){
+
+    fun autoLogin() {
         viewModelScope.launch {
-            if(!getTokenUseCase().isNullOrEmpty())
+            if (!getTokenUseCase().isNullOrEmpty()){
+                Constants.myToken = getTokenUseCase()!!
                 _autoLogin.value = true
+            }
         }
     }
 
@@ -53,15 +63,87 @@ class LoginViewModel @Inject constructor(
     ) {
         viewModelScope.launch {
             val result = firebaseAuth.signInWithCredential(credential).await().user
-            if (result?.uid != null){
+            if (result?.uid != null) {
                 //만약 처음 구글 로그인을 했다면 유저정보 설정 페이지로 이동
-                if(checkFirstUseCase(result.uid)){
+                if (checkFirstUseCase(result.uid)) {
                     moveToFirst(result.uid)
                 }
                 //이미 구글로그인한 기록이 있다면 홈 화면으로 이동
-                else{
+                else {
                     setTokenUseCase(result.uid)
+                    Constants.myToken = result.uid
                     moveToHome()
+                }
+            }
+        }
+    }
+
+    fun kakaoSignIn(
+        context: Context,
+        moveToHome: () -> Unit,
+        moveToFirst: (String) -> Unit
+    ) {
+        viewModelScope.launch {
+            val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+                if (error != null) {
+                    Log.e(TAG, "카카오계정으로 로그인 실패", error)
+                    showToastMessage(context, "카카오계정으로 로그인 실패")
+                } else if (token != null) {
+                    checkKakao(
+                        moveToHome=moveToHome,
+                        moveToFirst=moveToFirst
+                    )
+                }
+            }
+
+            UserApiClient.instance.me { user, error ->
+                Log.e(TAG, user.toString())
+            }
+
+// 카카오톡이 설치되어 있으면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인
+            if (UserApiClient.instance.isKakaoTalkLoginAvailable(context)) {
+                UserApiClient.instance.loginWithKakaoTalk(context) { token, error ->
+                    if (error != null) {
+                        Log.e(TAG, "카카오톡으로 로그인 실패", error)
+                        showToastMessage(context, "카카오계정으로 로그인 실패")
+                        // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
+                        // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
+                        if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
+                            return@loginWithKakaoTalk
+                        }
+
+                        // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인 시도
+                        UserApiClient.instance.loginWithKakaoAccount(context, callback = callback)
+                    } else if (token != null) {
+                        Log.i(TAG, "카카오톡으로 로그인 성공 ${token.accessToken}")
+                        checkKakao(
+                            moveToHome=moveToHome,
+                            moveToFirst=moveToFirst
+                        )
+                    }
+                }
+            } else {
+                UserApiClient.instance.loginWithKakaoAccount(context, callback = callback)
+            }
+        }
+    }
+
+    fun checkKakao(
+        moveToHome: () -> Unit,
+        moveToFirst: (String) -> Unit
+    ){
+        UserApiClient.instance.me { user, error ->
+            viewModelScope.launch {
+                if(user!=null){
+                    if (checkFirstUseCase(user.id.toString())) {
+                        moveToFirst(user.id.toString())
+                    }
+                    //이미 구글로그인한 기록이 있다면 홈 화면으로 이동
+                    else {
+                        setTokenUseCase(user.id.toString())
+                        Constants.myToken = user.id.toString()
+                        moveToHome()
+                    }
                 }
             }
         }
