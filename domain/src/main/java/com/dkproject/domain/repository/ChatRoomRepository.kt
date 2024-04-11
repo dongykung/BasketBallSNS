1e85300bf3d1bb1ef0f3a28283b47f7f320836fd
dongkyung <kdkd422@naver.com>
2024-04-11 23:55:08 +0900

    ChatRoomRepository

--- domain/src/main/java/com/dkproject/domain/repository/ChatRoomRepository.kt
+++ domain/src/main/java/com/dkproject/domain/repository/ChatRoomRepository.kt
@@ -1,8 +1,13 @@
 package com.dkproject.domain.repository
 
+import androidx.paging.PagingData
+import com.dkproject.domain.common.Resource
 import com.dkproject.domain.model.chat.ChatRoom
+import kotlinx.coroutines.flow.Flow
 
 interface ChatRoomRepository {
     suspend fun UploadChatRoom(myUid:String,chatRoom: ChatRoom) : String
     suspend fun getChatUidCheck(myUid:String,otherUid:String):String?
+
+    suspend fun getChatRooms(userUid:String): Flow<ChatRoom>
 }
