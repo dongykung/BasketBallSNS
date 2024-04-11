c38414f609fa9afa083eb3f11715076565e60416
dongkyung <kdkd422@naver.com>
2024-04-11 23:58:02 +0900

    ShopRepository

---
+++ domain/src/main/java/com/dkproject/domain/repository/ShopRepository.kt
@@ -0,0 +1,8 @@
+package com.dkproject.domain.repository
+
+import com.dkproject.domain.common.Resource
+import com.dkproject.domain.model.shop.Articles
+
+interface ShopRepository {
+    suspend fun getArticleInfo(uid:String):Resource<Articles>
+}
