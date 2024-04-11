3d81f90c6562efb78dafd4d5b8b60c2fbb1117a8
dongkyung <kdkd422@naver.com>
2024-04-11 23:56:49 +0900

    ProfileScreen

--- presentation/src/main/java/com/dkproject/presentation/ui/screen/home/profile/ProfileScreen.kt
+++ presentation/src/main/java/com/dkproject/presentation/ui/screen/home/profile/ProfileScreen.kt
@@ -1,26 +1,36 @@
 package com.dkproject.presentation.ui.screen.home.profile
 
 import android.content.Intent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.dkproject.presentation.ui.activity.LoginActivity
+import com.dkproject.presentation.ui.component.HomeTopAppBar
+import com.dkproject.presentation.ui.component.profile.UserInfoSection
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ProfileScreen(
     viewModel: ProfileViewModel = hiltViewModel()
 ) {
+    val state = viewModel.state.collectAsState().value
     val context = LocalContext.current
-    Button(onClick = {
-        viewModel.logOut {
-            context.startActivity(Intent(context, LoginActivity::class.java).apply {
-                flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-            })
+    Scaffold(topBar = {
+        CenterAlignedTopAppBar(title = { Text(text = state.userInfo.nickname)})
+    }) {innerPadding->
+        Column(modifier = Modifier.padding(innerPadding)) {
+            UserInfoSection(modifier=Modifier.fillMaxSize(),state.userInfo)
         }
-    }) {
-        Text(text = "logout")
     }
 }
