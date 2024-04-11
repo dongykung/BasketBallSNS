b3c906232038163eaf92c0cc34e08dfa507b2109
dongkyung <kdkd422@naver.com>
2024-04-11 23:55:00 +0900

    ChatRoomDTO

--- data/src/main/java/com/dkproject/data/model/ChatRoomDTO.kt
+++ data/src/main/java/com/dkproject/data/model/ChatRoomDTO.kt
@@ -1,8 +1,19 @@
 package com.dkproject.data.model
 
+import com.dkproject.domain.model.chat.ChatRoom
+
 data class ChatRoomDTO (
     val chatRoomId:String="",
     val lastMessage:String="",
     val lastMessageTime:Long=System.currentTimeMillis(),
     val otherUserUid:String="",
-)
+){
+    fun toDomainModel():ChatRoom{
+        return ChatRoom(
+            chatRoomId=chatRoomId,
+         lastMessage=lastMessage,
+         lastMessageTime=lastMessageTime,
+         otherUserUid=otherUserUid,
+        )
+    }
+}
