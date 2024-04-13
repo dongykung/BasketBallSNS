72fa7799caea46deec5dc02c125c2888dc7ccb0e
dongkyung <kdkd422@naver.com>
2024-04-13 14:05:21 +0900

    상품 의존성 주입

--- data/src/main/java/com/dkproject/data/di/ShopModule.kt
+++ data/src/main/java/com/dkproject/data/di/ShopModule.kt
@@ -4,6 +4,7 @@ import com.dkproject.data.repository.ShopRepositoryImpl
 import com.dkproject.domain.repository.ChatRepository
 import com.dkproject.domain.repository.ShopRepository
 import com.dkproject.domain.usecase.chat.UploadChatUseCase
+import com.dkproject.domain.usecase.shop.DeleteArticleUseCase
 import com.dkproject.domain.usecase.shop.GetArticleItemUseCase
 import com.google.firebase.firestore.FirebaseFirestore
 import dagger.Module
@@ -26,5 +27,11 @@ object ShopModule {
     @Singleton
     fun providegetArticleItemUseCase(shopRepository: ShopRepository): GetArticleItemUseCase {
         return GetArticleItemUseCase(shopRepository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideDeleteArticleItemUseCase(shopRepository: ShopRepository): DeleteArticleUseCase {
+        return DeleteArticleUseCase(shopRepository)
     }
 }
