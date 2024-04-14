c612bbfd85b0c60929cb4677257dc5d2b192bb82
dongkyung <kdkd422@naver.com>
2024-04-14 09:47:05 +0900

    테마

--- presentation/src/main/java/com/dkproject/presentation/ui/theme/Type.kt
+++ presentation/src/main/java/com/dkproject/presentation/ui/theme/Type.kt
@@ -1,6 +1,7 @@
 package com.dkproject.presentation.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
@@ -10,6 +11,9 @@ import com.dkproject.presentation.R
 
 val roboto = FontFamily(
    Font(R.font.roboto_medium)
+)
+val bebasneue = FontFamily(
+    Font(R.font.bungeespice_regular)
 )
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -26,6 +30,13 @@ val Typography = Typography(
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = bebasneue,
+        fontWeight = FontWeight.Bold,
+        fontSize = 46.sp,
+        letterSpacing = 1.6.sp,
+        color = Color(255,119,77)
     )
 
     /* Other default text styles to override
