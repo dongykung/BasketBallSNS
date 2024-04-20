669b24918faf5fc3fd77b578dfba03a86563db04
dongkyung <kdkd422@naver.com>
2024-04-20 18:50:43 +0900

    토스트 메시지 두번 출력되던 어류 수정

--- domain/src/main/java/com/dkproject/domain/usecase/home/ApplyCancelUseCase.kt
+++ domain/src/main/java/com/dkproject/domain/usecase/home/ApplyCancelUseCase.kt
@@ -8,7 +8,6 @@ import kotlinx.coroutines.flow.flow
 class ApplyCancelUseCase(private val guestRepository: GuestRepository) {
     suspend operator fun invoke(guestItemUid:String,updateList:List<String>) : Flow<Resource<Boolean>> = flow {
        try {
-           emit(Resource.Loading())
            emit(guestRepository.applyCancel(guestItemUid,updateList))
        }catch (e:Exception){
            emit(Resource.Error(e.message.toString()))
